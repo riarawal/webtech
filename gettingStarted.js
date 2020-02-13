41ad0b6a8c6ebd3b566fe77ba0941ec99e604a5f
riarawal <54265567+riarawal@users.noreply.github.com>
2020-02-13 11:49:02 +0530

    Add files via upload

---
+++ gettingStarted.js
@@ -0,0 +1,15 @@
+for(var i=1; i<=20;i++)
+{
+    if (i%3==0 && i%5!=0){
+        console.log("fizz");
+    }
+    else if (i%5==0 && i%3!=0){
+        console.log("buzz");
+    }
+    else if (i%3==0 && i%5==0){
+        console.log("fizzbuzz");
+    }
+    else{
+        console.log(i);
+    }
+}
