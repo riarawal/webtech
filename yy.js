90456d476aa4dcb2bbc180f17508adaf4077fab6
riarawal <54265567+riarawal@users.noreply.github.com>
2020-02-15 12:08:10 +0530

    Add files via upload

---
+++ yy.js
@@ -0,0 +1,10 @@
+const arr= [5,8,14,17,23,6,8];
+function isOdd(x){
+    return x % 2 ==1;
+}
+ function includer(arr, isOdd){
+     const narr=[];
+     for (let i=0; i<arr.length;i++){
+         if(isOdd(arr))
+     }
+ }
