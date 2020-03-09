74cbd13a323d66627a7f945872705f4b08aba09d
riarawal <54265567+riarawal@users.noreply.github.com>
2020-03-09 23:00:45 +0530

    Update animal.js

--- animal.js
+++ animal.js
@@ -1,10 +1,11 @@
 var animals=['Hen','Elephant','Tiger','Dog','Cat','Lion'];
 //Array of animals
+
 function ani(animals)
 {
     return animals[0];
 }
-
+//Use of map shown
 const saa=animals.map(ani);
 console.log(saa.join(""));
 
