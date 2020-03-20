68fe636ca58f4cfe44ee68928e4821006fff3428
riarawal <54265567+riarawal@users.noreply.github.com>
2020-03-20 23:39:57 +0530

    Update reverseArr.js

--- reverseArr.js
+++ reverseArr.js
@@ -1,9 +1,11 @@
 var first = [1, 2, 3, 4, 5];
 var rArr = [];
 var length=first.length;
+
 for (var i = 0; i <length; i++) 
 {
 console.log(i + " " + first.length);
 rArr.push(first.pop());
 }
+
 console.log(rArr);
